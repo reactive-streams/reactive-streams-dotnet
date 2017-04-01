ee890a0cb2d08b4f350821bee62ee62598d70e24
Marc Piechura <piechura@gmx.net>
2017-04-01 21:09:42 +0200

    restore nullable whitebox test

--- src/examples/Reactive.Streams.Example.Unicast.Tests/SyncSubscriberWhiteboxTest.cs
+++ src/examples/Reactive.Streams.Example.Unicast.Tests/SyncSubscriberWhiteboxTest.cs
@@ -5,9 +5,9 @@ using Reactive.Streams.TCK;
 namespace Reactive.Streams.Example.Unicast.Tests
 {
     [TestFixture]
-    public class SyncSubscriberWhiteboxTest : SubscriberWhiteboxVerification<int>
+    public class ValueTypeSyncSubscriberWhiteboxTest : SubscriberWhiteboxVerification<int>
     {
-        public SyncSubscriberWhiteboxTest() : base(new TestEnvironment())
+        public ValueTypeSyncSubscriberWhiteboxTest() : base(new TestEnvironment())
         {
         }
 
@@ -52,6 +52,69 @@ namespace Reactive.Streams.Example.Unicast.Tests
             }
 
             protected override bool WhenNext(int element) => true;
+
+            public override void OnError(Exception cause)
+            {
+                base.OnError(cause);
+                _probe.RegisterOnError(cause);
+            }
+
+            public override void OnComplete()
+            {
+                base.OnComplete();
+                _probe.RegisterOnComplete();
+            }
+        }
+    }
+
+    [TestFixture]
+    public class NullableSyncSubscriberWhiteboxTest : SubscriberWhiteboxVerification<int?>
+    {
+        public NullableSyncSubscriberWhiteboxTest() : base(new TestEnvironment())
+        {
+        }
+
+        public override int? CreateElement(int element) => element;
+
+        public override ISubscriber<int?> CreateSubscriber(WhiteboxSubscriberProbe<int?> probe) => new Subscriber(probe);
+
+        private sealed class Subscriber : SyncSubscriber<int?>
+        {
+            private readonly WhiteboxSubscriberProbe<int?> _probe;
+
+            public Subscriber(WhiteboxSubscriberProbe<int?> probe)
+            {
+                _probe = probe;
+            }
+
+            public override void OnSubscribe(ISubscription subscription)
+            {
+                base.OnSubscribe(subscription);
+
+                _probe.RegisterOnSubscribe(new SubscriberPuppet(subscription));
+            }
+
+            private sealed class SubscriberPuppet : ISubscriberPuppet
+            {
+                private readonly ISubscription _subscription;
+
+                public SubscriberPuppet(ISubscription subscription)
+                {
+                    _subscription = subscription;
+                }
+
+                public void TriggerRequest(long elements) => _subscription.Request(elements);
+
+                public void SignalCancel() => _subscription.Cancel();
+            }
+
+            public override void OnNext(int? element)
+            {
+                base.OnNext(element);
+                _probe.RegisterOnNext(element);
+            }
+
+            protected override bool WhenNext(int? element) => true;
 
             public override void OnError(Exception cause)
             {
